590b8d85475d73a37a69153ef6f546991271ab17
jeanl <jeanl@LAPTOP-PV81UB2O.mshome.net>
2020-09-29 14:57:48 +0200

    mise en place des tests FunctionalException

--- myerp-technical/src/test/java/com/dummy/myerp/technical/exception/FunctionalExceptionTest.java
+++ myerp-technical/src/test/java/com/dummy/myerp/technical/exception/FunctionalExceptionTest.java
@@ -2,6 +2,10 @@ package com.dummy.myerp.technical.exception;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 
 
@@ -11,8 +15,17 @@ import org.junit.Test;
 public class FunctionalExceptionTest {
 	
 	
+	
+	
+	
 	@Test
-	public void checkFunctionalException() {
+	public void checkMessageFunctionalException() {
+		Assert.assertTrue("Le message de l'exception a été correctement envoyé", new FunctionalException("pMessage").getMessage().equals(new Exception("pMessage").getMessage()));
+	}
+	
+	
+	@Test
+	public void checkCauseFunctionalException() {
 		Exception exception = new Exception();
 		Throwable pcause = new Throwable();
 		exception.initCause(pcause);
@@ -21,6 +34,19 @@ public class FunctionalExceptionTest {
 		
 		
 		Assert.assertTrue("Problème dans la reconnaissance de l'exception par FunctionalException", functionalException.getCause().toString().equals(exception.getCause().toString())); 
+		
+	}
+	
+	@Test
+	public void checkCauseAndMessageFunctionalException() {
+		Throwable pcause = new Throwable();
+		Exception exception = new Exception();
+		FunctionalException functionalException = new FunctionalException("pMessage", pcause);
+		exception.initCause(pcause);
+		
+		Assert.assertTrue("Le message de l'exception a été correctement envoyé", new FunctionalException("pMessage").getMessage().equals(new Exception("pMessage").getMessage()));
+		Assert.assertTrue("Problème dans la reconnaissance de l'exception par FunctionalException", functionalException.getCause().toString().equals(exception.getCause().toString())); 
+		
 		
 	}
 
